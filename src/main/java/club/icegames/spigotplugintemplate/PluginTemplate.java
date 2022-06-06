739475b77b40ce9061713efe8e9cffceeb23186a
Seailz <81972974+IoyoCode@users.noreply.github.com>
2022-06-06 04:45:46 +0100

    cleaned up a lot of code

--- src/main/java/club/icegames/spigotplugintemplate/PluginTemplate.java
+++ src/main/java/club/icegames/spigotplugintemplate/PluginTemplate.java
@@ -2,13 +2,9 @@ package club.icegames.spigotplugintemplate;
 
 import club.icegames.spigotplugintemplate.core.Locale;
 import club.icegames.spigotplugintemplate.core.Logger;
-import club.icegames.spigotplugintemplate.core.license.ULicense;
-import club.icegames.spigotplugintemplate.core.utils.ConfigUtils;
 import games.negative.framework.BasePlugin;
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 
 public final class PluginTemplate extends BasePlugin {
 
@@ -23,14 +19,6 @@ public final class PluginTemplate extends BasePlugin {
         long start = System.currentTimeMillis();
 
         setInstance(this);
-
-        FileConfiguration license = new ConfigUtils("license").getConfig();
-
-        if (!new ULicense(this, license.getString("license-key"), "https://licenses.seailz.com/api/client", "cac79c8e128cd775297fb454516b6c2c2b0cfdd9").verify()) {
-            Bukkit.getPluginManager().disablePlugin(this);
-            Bukkit.getScheduler().cancelTasks(this);
-            return;
-        }
 
         // Set details and register things
         register(RegisterType.COMMAND);
